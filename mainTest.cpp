7c695e0b5bfa3c0e10ccdde22f6fdafb9c98a216
filippob04 <baldinifilippo4.commercial@gmail.com>
2025-05-30 18:44:09 +0200

    howManyItem funziona se non ci sono prodotti con dipendenze comuni. da implementare nuovamente

--- mainTest.cpp
+++ mainTest.cpp
@@ -110,14 +110,63 @@ int main() {
     else cout << "  Result: FAILED\n\n";
 
     cout << "*************** Test " << i++ << ": howManyItem 'motore' ****************" << endl;
+
     try {
-        bres = howManyItem(indus, "motore", ires);
-        breq = true;
-        ireq = 50; // 50 bulloni + 50 acciaio ⇒ 50 motori
-        if (bres == breq && ires == ireq)
-            cout << "  Result: PASSED (produced " << ires << ")\n\n";
+        unsigned resFerroPrima = 0, resCarbonioPrima = 0, resAcciaioPrima = 0, resBullonePrima = 0;
+        unsigned resMotore = 0;
+        unsigned resFerroDopo = 0, resCarbonioDopo = 0, resAcciaioDopo = 0, resBulloneDopo = 0;
+
+        if(howManyItem(indus, "ferro", resFerroPrima))
+            cout << "Ferro disponibile prima: " << resFerroPrima << endl;
         else
-            cout << "  Result: FAILED (produced " << ires << ", expected " << ireq << ")\n\n";
+            cout << "Errore calcolo ferro prima" << endl;
+
+        if(howManyItem(indus, "carbonio", resCarbonioPrima))
+            cout << "Carbonio disponibile prima: " << resCarbonioPrima << endl;
+        else
+            cout << "Errore calcolo carbonio prima" << endl;
+
+        if(howManyItem(indus, "acciaio", resAcciaioPrima))
+            cout << "Acciaio disponibile prima: " << resAcciaioPrima << endl;
+        else
+            cout << "Errore calcolo acciaio prima" << endl;
+
+        if(howManyItem(indus, "bullone", resBullonePrima))
+            cout << "Bulloni disponibili prima: " << resBullonePrima << endl;
+        else
+            cout << "Errore calcolo bullone prima" << endl;
+
+        bres = howManyItem(indus, "motore", resMotore);
+        breq = true;
+        ireq = 50; // aspettati 50 motori
+
+        cout << "Motori producibili: " << resMotore << endl;
+
+        if(howManyItem(indus, "ferro", resFerroDopo))
+            cout << "Ferro disponibile dopo: " << resFerroDopo << endl;
+        else
+            cout << "Errore calcolo ferro dopo" << endl;
+
+        if(howManyItem(indus, "carbonio", resCarbonioDopo))
+            cout << "Carbonio disponibile dopo: " << resCarbonioDopo << endl;
+        else
+            cout << "Errore calcolo carbonio dopo" << endl;
+
+        if(howManyItem(indus, "acciaio", resAcciaioDopo))
+            cout << "Acciaio disponibile dopo: " << resAcciaioDopo << endl;
+        else
+            cout << "Errore calcolo acciaio dopo" << endl;
+
+        if(howManyItem(indus, "bullone", resBulloneDopo))
+            cout << "Bulloni disponibili dopo: " << resBulloneDopo << endl;
+        else
+            cout << "Errore calcolo bullone dopo" << endl;
+
+        if (bres == breq && resMotore == ireq)
+            cout << "  Result: PASSED (produced " << resMotore << ")\n\n";
+        else
+            cout << "  Result: FAILED (produced " << resMotore << ", expected " << ireq << ")\n\n";
+
     } catch (...) {
         cout << "  Result: FAILED (exception thrown)\n\n";
     }
